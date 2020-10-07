e5b6c9bd5dc7c50b79e0fba1ecdd5f208b2e78e6
Tom Keffer <tkeffer@gmail.com>
2020-10-07 16:14:01 -0700

    Added tag $time_at()

--- bin/user/time_since.py
+++ bin/user/time_since.py
@@ -4,29 +4,28 @@
 #    See the file LICENSE.txt for your full rights.
 #
 
-"""Search list extension to calculate elapsed time since a SQL statement evaluated true.
+"""Search list extension to calculate when an SQL statement last evaluted true, or how long since it evaluated true.
 
 Example:
 
-    <p>It has been $time_since('rain>0') since it last rained.</p>
+    <p>It last rained at $time_at('rain>0') ($time_since('rain>0') ago).</p>
 
 would result in
 
-    <p>It has been 81 days, 1 hour, 35 minutes since it last rained.</p>
+    <p>It last rained 20 June 2020 (81 days, 1 hour, 35 minutes ago).</p>
 
 """
 from weewx.cheetahgenerator import SearchList
 
 from weewx.units import ValueTuple, ValueHelper
 
-VERSION = "0.1"
+VERSION = "0.2"
 
 
 class TimeSince(SearchList):
-    """Time since a sql expression evaluted true"""
-
     def get_extension_list(self, timespan, db_lookup):
-        def calc(expression):
+        def time_since(expression):
+            """Time since a sql expression evaluted true"""
             db_manager = db_lookup()
             sql_stmt = "SELECT dateTime FROM %s WHERE %s AND dateTime <= %d ORDER BY dateTime DESC LIMIT 1" \
                        % (db_manager.table_name, expression, timespan.stop)
@@ -37,4 +36,19 @@ class TimeSince(SearchList):
             vh = ValueHelper(vt, formatter=self.generator.formatter, converter=self.generator.converter)
             return vh
 
-        return [{'time_since': calc}]
+        def time_at(expression):
+            """When an sql expression evaluated true"""
+            db_manager = db_lookup()
+            sql_stmt = "SELECT dateTime FROM %s WHERE %s AND dateTime <= %d ORDER BY dateTime DESC LIMIT 1" \
+                       % (db_manager.table_name, expression, timespan.stop)
+
+            row = db_manager.getSql(sql_stmt)
+            val = row[0] if row else None
+            vt = ValueTuple(val, 'unix_epoch', 'group_time')
+            vh = ValueHelper(vt, formatter=self.generator.formatter, converter=self.generator.converter)
+            return vh
+
+        return [{
+            'time_since': time_since,
+            'time_at': time_at,
+        }]
